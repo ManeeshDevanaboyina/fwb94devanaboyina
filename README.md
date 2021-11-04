ba87061bc627e3696108ce0cfd27d9d05fc1c273
ManeeshDevanaboyina <32562977+ManeeshDevanaboyina@users.noreply.github.com>
2021-11-03 22:49:06 -0500

    Updated changes  and changed

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 Working on pug templates to dynamically generate web pages on the server side.
 Hosted at <https://fwb94devanaboyina.herokuapp.com/>
 
-Class - Mask class Mask{ constructor(color, price, size){ this.color = color; this.price = price; this.size = size; } }
+Class - mask class mask{ constructor(color, price, size){ this.color = color; this.price = price; this.size = size; } }
--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var maskRouter = require('./routes/mask');
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/mask', maskRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
